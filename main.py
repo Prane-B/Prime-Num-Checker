ac0892063f9f8909471fc4c4e58bb5edd810fd49
Prane-B <pranesh.npsi@gmail.com>
2024-09-27 16:41:51 +0530

    beta release 1

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-numstart = input('start digit:')
-numend = input('end digit')
+num = int(input('Enter a number'))
+for i in range(0,num):
+    if num % i == 0:
+        print('is not a prime number can be divided with',i)
+        break
+    else:
+        print("is a prime number")
 
